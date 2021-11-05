7bdf58f5ae37eca631a13824aa7a5b762dd39269
Peter Bartlog <peter.bartlog@gmail.com>
2021-11-05 14:07:09 +0100

    Merge branch 'S-Box-diff-logic'

--- slogic.py
+++ slogic.py
@@ -9,3 +9,13 @@ def importSBox(file):
     return s_box
 
                
+def calculate_ddt(SBox_file: csv = 'SBox/SBox1.csv',delta_E_range: int = 32,delta_A_range: int = 32) -> list:
+    ddt = [[[] for x in range(delta_A_range)] for y in range(delta_E_range)]
+    s_box = importSBox(SBox_file)
+    for a in range(delta_A_range):
+        for e in range(delta_E_range):
+            delta_E: int = a ^ e
+            delta_A: int = s_box[a] ^ s_box[e]
+            ddt[delta_A][delta_E].append((e,a))
+    return ddt
+
