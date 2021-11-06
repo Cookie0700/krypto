04bb7f091f53c0b7fe251d468c9602d4662a72df
Cookie0700 <marcphilipp.mueller@gmail.com>
2021-11-06 09:30:15 +0100

    ddt call

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from slogic import importSBox
+from slogic import calculate_ddt, importSBox
 from krypto import encrypt
 
 s1 = importSBox('SBox/SBox1.csv')
@@ -8,3 +8,5 @@ s2 = importSBox('SBox/SBox2.csv')
 #print(s2)
 
 print(encrypt(s1, s2))
+
+print(calculate_ddt())
