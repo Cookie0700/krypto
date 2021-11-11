27087f1ebc51b8e4f6c1fc2391f80f6571277493
Kevin Kaupp <kevin.kaupp@hotmail.de>
2021-11-11 11:44:25 +0100

    Squashed commit of the following:
    commit 46f652fc5e83e331372c4f086a2806d718ed9804
    Author: Kevin Kaupp <kevin.kaupp@hotmail.de>
    Date:   Thu Nov 11 11:38:54 2021 +0100
    
        added count_linear

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ from slogic import calculate_ddt, importSBox, size_of_ddt
 from output_ddt import test
 from slogic import calculate_ddt, importSBox
 from krypto import encrypt
-from linear import is_not_linear
+from linear import is_not_linear, count_linear
 
 s1 = importSBox('SBox/SBox1.csv')
-#print(s1)
+print(is_not_linear(s1))
+count_linear(s1)
 
 s2 = importSBox('SBox/SBox2.csv')
-#print(s2)
+print(is_not_linear(s2))
+count_linear(s2)
 
 s3 = importSBox('SBox/SBox3.csv')
 #print(encrypt(s1, s2))
 
 
 print(is_not_linear(s3))
+count_linear(s3)
+
+# print(is_not_linear(s1))
+count_linear(s3)
 ddt = calculate_ddt(SBox_file='SBox/SBox1.csv')
 ddt_size = size_of_ddt(ddt)
 
