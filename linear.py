15437a1bea5f5a2dae4acfefd8424bc6868026db
Kevin Kaupp <kevin.kaupp@hotmail.de>
2021-11-11 10:37:21 +0100

    for marc, in love kevin

--- linear.py
+++ linear.py
@@ -1,32 +1,11 @@
-# Nicht-linear = Nicht additiv/homogen
-# 110  = S1(011) = S1(101 + 110) /= S1(101) + S1(110) = 111 + 101 = 010
-# S1(00000) -> 
-
 import csv
+# 110  = S1(011) = S1(101 + 110) /= S1(101) + S1(110) = 111 + 101 = 010
 
-def linear_check(file):
-    with open(file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            print(row)
-            # Input = {row[0]}
-            # Output = {row[1]}
-
-# linear_check('bits.csv')
-
-def binary_combinations(bit_length): 
-    for i in range(2**bit_length):
-        s="{:05b}".format(i)
-        print(s)
-        
-binary_combinations(5)
-
-def test():
-    a = '001'
-    b = '011'
-    c = int(a,2) + int(b,2)
-    print(c)
-
-# test()
-
-# linear_check('SBox/SBox1.csv')
+def is_linear(sbox): 
+    for i in range(32):
+        for j in range(32): 
+            # print(i ^ j)
+            if sbox[i ^ j] == (sbox[i] ^ sbox[j]):
+                print(i, j)
+                return True
+    return False
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from slogic import calculate_ddt, importSBox, size_of_ddt
 from krypto import encrypt
+from linear import is_linear
 
 s1 = importSBox('SBox/SBox1.csv')
 #print(s1)
@@ -7,8 +8,10 @@ s1 = importSBox('SBox/SBox1.csv')
 s2 = importSBox('SBox/SBox2.csv')
 #print(s2)
 
+s3 = importSBox('SBox/SBox11.csv')
 #print(encrypt(s1, s2))
 
+print(is_linear(s3))
 ddt = calculate_ddt(SBox_file='SBox/SBox1.csv')
 ddt_size = size_of_ddt(ddt)
 
