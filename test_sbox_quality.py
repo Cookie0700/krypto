5a5e2f2755dff4d426c8fec325d1a425584b7070
Peter Bartlog <peter.bartlog@gmail.com>
2021-11-15 11:41:33 +0100

    Merge commit '74e4c78b66aa64b004a11a7d4af27590ecf52908'

--- test_sbox_quality.py
+++ test_sbox_quality.py
@@ -1,22 +1,23 @@
 import os
 
-from slogic import importSBox, calculate_ddt, size_of_ddt
+from slogic import calculate_ddt, size_of_ddt
 
 
 def get_worst_sbox(myFolder = 'SBox'):
     fileset = [root+'/'+filename for root, _, files in os.walk(myFolder) for filename in files]
 
-    #print(fileset)
     highest_differential_count: int = 0
-    worst_sbox: str 
+    worst_sbox = []
     for file in fileset:
-        #sbox = importSBox(file)
         sbox_ddt_size = size_of_ddt(calculate_ddt(file))
         for row in sbox_ddt_size:
             for differential_count in row:
-                if differential_count > highest_differential_count:
+                if differential_count >= highest_differential_count:
                     highest_differential_count = differential_count
-                    worst_sbox = file
-    return worst_sbox, highest_differential_count
+                    worst_sbox.append((file,highest_differential_count))
+        for index, (filename,highest_differential) in enumerate(worst_sbox):
+            if highest_differential < highest_differential_count:
+                del worst_sbox[index]
+    return worst_sbox
 
 print(get_worst_sbox('SBox/Random_SBox'))
