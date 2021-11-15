0151365812b13aeb1c64ab1f1b262cb7520578b8
Cookie0700 <marcphilipp.mueller@gmail.com>
2021-11-15 11:20:47 +0100

    analysed 151.csv

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from slogic import calculate_ddt, importSBox, distribution_percentage, size_of_d
 from krypto import encrypt
 from linear import is_not_linear, count_linear
 
-s1 = importSBox('SBox/SBox1.csv')
-print(is_not_linear(s1))
-count_linear(s1)
+# s1 = importSBox('SBox/SBox1.csv')
+# print(is_not_linear(s1))
+# count_linear(s1)
 
-s2 = importSBox('SBox/SBox2.csv')
-print(is_not_linear(s2))
-count_linear(s2)
+# s2 = importSBox('SBox/SBox2.csv')
+# print(is_not_linear(s2))
+# count_linear(s2)
 
-# s3 = importSBox('SBox/SBox3.csv')
-# print(is_not_linear(s3))
-# count_linear(s3)
+s3 = importSBox('SBox/Random_SBox/151.csv')
+print(is_not_linear(s3))
+count_linear(s3)
 
-ddt = calculate_ddt(SBox_file='SBox/SBox1.csv')
+ddt = calculate_ddt(SBox_file='SBox/Random_SBox/151.csv')
 ddt_size = size_of_ddt(ddt)
 
 csv_print(ddt)
