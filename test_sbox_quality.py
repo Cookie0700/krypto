d9c639642ef0add571202dcba2e51e091f0c2ab7
Peter Bartlog <peter.bartlog@gmail.com>
2021-11-15 11:32:49 +0100

    improved sbox quality test, now prints list

--- test_sbox_quality.py
+++ test_sbox_quality.py
@@ -1,6 +1,6 @@
 import os
 
-from slogic import importSBox, calculate_ddt, size_of_ddt
+from slogic import calculate_ddt, size_of_ddt
 
 
 def get_worst_sbox(myFolder = 'SBox'):
@@ -8,15 +8,15 @@ def get_worst_sbox(myFolder = 'SBox'):
 
     #print(fileset)
     highest_differential_count: int = 0
-    worst_sbox: str 
+    worst_sbox = []
     for file in fileset:
         #sbox = importSBox(file)
         sbox_ddt_size = size_of_ddt(calculate_ddt(file))
         for row in sbox_ddt_size:
             for differential_count in row:
-                if differential_count > highest_differential_count:
+                if differential_count >= highest_differential_count:
                     highest_differential_count = differential_count
-                    worst_sbox = file
-    return worst_sbox, highest_differential_count
+                    worst_sbox.append((file,highest_differential_count))
+    return worst_sbox
 
 print(get_worst_sbox('SBox/Random_SBox'))
